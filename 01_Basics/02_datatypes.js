dd3aa9561520d64e62adfe2cfffddfc7ec0ca6fe
archanakumari1234 <archanak233226@gmail.com>
2023-08-08 02:59:36 +0530

    Create 02_datatypes.js

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS as newer version
+
+// alert(2+2)   //it will not work because we are using nodejs, not browser
+
+let name = "Archana"   // string datatype
+let age = 20           // number
+let isLoggedIn = false    // boolean 
+
+//  Primitive datatype
+
+// number => 2 to power 53 range
+// bigint
+// string => in the ""
+// boolean => true/false
+// null => standalone value
+// undefined => we define the variable but not give any value
+// symbol => unique
+
+console.log(typeof name); // output: string
+console.log(typeof age);            // output:  number
+console.log(typeof isLoggedIn);    // output: boolean
+console.log(typeof undefined);    // output:  undefined
+console.log(typeof null);    // output:
