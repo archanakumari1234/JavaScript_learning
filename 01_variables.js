7c62cb66896a7c9e7bb28615d23393585d2f0da3
archanakumari1234 <archanak233226@gmail.com>
2023-08-08 00:52:53 +0530

    Added about

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 123344;
+let accountEmail = "archana12@gmail.com";
+var accountPassword = "12121212";
+accountCity = "Jaipur";
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+console.table([accountId, accountEmail, accountPassword, accountCity])
