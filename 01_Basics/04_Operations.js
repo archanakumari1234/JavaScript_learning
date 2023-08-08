e5b4be4bedf41264c93aecab7762cc9e7989709c
archanakumari1234 <archanak233226@gmail.com>
2023-08-08 11:53:40 +0530

    variables

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,53 @@
+// **************** Operations *************
+
+// **************** Additional Operations *************
+console.log(2+2);
+console.log(4-2);
+console.log(2*2);
+console.log(8/2);
+console.log(2**2);
+console.log(3%2);
+
+let str1 = "Archana"
+let str2 = " Kumari"
+let str = str1 + str2
+console.log(str);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "1");
+console.log(1 + "1" + 2);
+
+console.log(true);
+console.log(+true);
+console.log(-true);
+console.log(false);
+console.log(+"");
+
+let gameCounter = 50
+console.log(gameCounter++);
+console.log(++gameCounter);
+console.log(gameCounter--);
+console.log(--gameCounter);
+
+// **************** Comparison operation *************
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(1 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1); // output: true
+console.log("1" < 2); // output: true
+
+console.log(null > 0); // output: false
+console.log(null < 2); // output: true
+console.log(null >= 0); // output: true
+
+console.log(undefined == 0); // output: false
+console.log(undefined < 2); // output: false
+console.log(undefined >= 0); // output: false
+console.log(2==="2"); // output: false
+console.log(2===2); // output: true
