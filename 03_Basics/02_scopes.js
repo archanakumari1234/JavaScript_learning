7ced897be8d4e6c7069a140b67e1ec613a7e1425
archanakumari1234 <archanak233226@gmail.com>
2023-08-11 15:24:45 +0530

    scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,60 @@
+
+
+if(true){
+    let a = 1200
+    console.log(a);
+}
+// console.log(a);
+
+if(true){
+    const b = 1000
+    console.log(b);
+}
+// console.log(b);
+
+if(true){
+    var c = 2000    // its global scop variable
+    // console.log(c);
+}
+console.log(c);
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+function one(){
+    const username = "Archana"
+    function two(){
+        const website = "YouTube"
+        console.log(website);
+    }
+    console.log(username);
+    two();
+}
+one();
+// +++++++++++++++++++++++++++++++++++++++++++
+if(true){
+    const username = "Khushi"
+    if(true){
+        const website = " instagram"
+        console.log(username + website);
+    }
+}
+
+
+// ++++++++++ mini hoisting +++++++++
+console.log(add(5))
+function add(num){
+   return num+3
+}
+// console.log(add(5))
+//++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(afunction(5));
+const afunction = function add(num){
+    return num+7
+ }
+ console.log(afunction(5));
+
+
+
+
+
+
